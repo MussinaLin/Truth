be748988b542c1edb7cd1249e056ee3ebfcf88d1
Mussina Lin <mussinalin@gmail.com>
2023-07-07 13:18:44 +0800

    feat: add remapping

--- src/Truth.sol
+++ src/Truth.sol
@@ -1,8 +1,7 @@
 pragma solidity ^0.8.0;
 
-import "filename" from 
-contract Truth is  {
-    constructor() {
-        
-    }
+import {ERC721} from 'openzeppelin-contracts/contracts/token/ERC721/ERC721.sol';
+
+contract Truth is ERC721 {
+    constructor() ERC721('Truth', 'Truth') {}
 }
